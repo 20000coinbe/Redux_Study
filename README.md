44dd12162f406eb2305be03a1aeb32b20428f16c
neo <kimdks12@gmail.com>
2022-11-02 18:38:48 +0900

    [apply redux-actions(filter.js)]

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 - 비교를 위해서 reducers폴더의 파일명을 변경해놓은 상태
 
+2022-11-02 Redux-saga 및 redux-actions 적용
+
+- 중요!! thunk와 saga 비교
+
 https://cute-thyme-1f4.notion.site/Forging-React-Redux-3d236cc08d4947ff823917d7d28463d9
--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,34 +1,55 @@
-// 액션 타입 정의
-const SHOW_ALL = "redux-start/filter/SHOW_ALL";
-const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
-
-// 액션 생성 함수
-export function showAll() {
-  return {
-    type: SHOW_ALL,
-  };
-}
-
-export function showComplete() {
-  return {
-    type: SHOW_COMPLETE,
-  };
-}
+import { createActions, handleActions } from "redux-actions";
 
 // 초기값
 const initialState = "ALL";
 
+export const { showAll, showComplete } = createActions(
+  "SHOW_ALL",
+  "SHOW_COMPLETE",
+  {
+    prefix: "redux-start/filter",
+  }
+);
+
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => "ALL",
+    SHOW_COMPLETE: () => "COMPLETE",
+  },
+  initialState,
+  { prefix: "redux-start/filter" }
+);
+
+export default reducer;
+
+// 액션 타입 정의
+// const SHOW_ALL = "redux-start/filter/SHOW_ALL";
+// const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
+
+// 액션 생성 함수
+// export function showAll() {
+//   return {
+//     type: SHOW_ALL,
+//   };
+// }
+
+// export function showComplete() {
+//   return {
+//     type: SHOW_COMPLETE,
+//   };
+// }
+
 // 리듀서
-export default function reducer(previousState = initialState, action) {
-  // 형태 : { todos: [{ text: '코딩', done: false }, { text: "설거지", done: false }], filter: "ALL"}
+// export default function reducer(previousState = initialState, action) {
+//   // 형태 : { todos: [{ text: '코딩', done: false }, { text: "설거지", done: false }], filter: "ALL"}
 
-  if (action.type === SHOW_COMPLETE) {
-    return "COMPLETE";
-  }
+//   if (action.type === SHOW_COMPLETE) {
+//     return "COMPLETE";
+//   }
 
-  if (action.type === SHOW_ALL) {
-    return "ALL";
-  }
+//   if (action.type === SHOW_ALL) {
+//     return "ALL";
+//   }
 
-  return previousState;
-}
+//   return previousState;
+// }
